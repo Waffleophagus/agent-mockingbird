a12d2a5ffc4e6a55c2c109fadbfb34f9e25fe4d2
Matt Campbell <matt@battleshopper.com>
2026-03-09 02:11:10 -0700

    Refactor Streamdown package paths and update configurations
    - Updated `.gitignore` to remove exceptions for Streamdown distribution directories, streamlining package management.
    - Introduced a new `.npmrc` file to specify the Streamdown package registry.
    - Modified `bun.lock` and `package.json` to replace local file references with versioned package dependencies for Streamdown components.
    - Adjusted TypeScript configurations across applications to remove outdated paths for Streamdown packages, ensuring cleaner module resolution.
    - Cleaned up `metro.config.js` in the mobile app by removing unnecessary path configurations.

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,30 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const path = require("path");
 
 const config = getDefaultConfig(__dirname);
-const streamdownRoot = "/var/home/matt/Documents/random-vibecoded-stuff/streamdown";
-const appNodeModules = path.resolve(__dirname, "node_modules");
-const rootNodeModules = path.resolve(__dirname, "../../node_modules");
-
-config.watchFolders = [...(config.watchFolders ?? []), streamdownRoot];
-config.resolver.unstable_enableSymlinks = true;
-config.resolver.nodeModulesPaths = [
-  appNodeModules,
-  rootNodeModules,
-];
-config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules ?? {}),
-  react: path.join(appNodeModules, "react"),
-  "react-native": path.join(appNodeModules, "react-native"),
-  "react-native-reanimated": path.join(appNodeModules, "react-native-reanimated"),
-  "react-native-worklets": path.join(appNodeModules, "react-native-worklets"),
-};
-config.resolver.blockList = [
-  new RegExp(
-    `${streamdownRoot.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}\\/node_modules\\/.*`
-  ),
-];
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
